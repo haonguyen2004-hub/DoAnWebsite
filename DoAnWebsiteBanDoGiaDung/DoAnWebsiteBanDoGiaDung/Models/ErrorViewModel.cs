04db13ce36c6036cdaa1d80b02c45b28e70d2e1d
haonguyen2004-hub <147829112+haonguyen2004-hub@users.noreply.github.com>
2024-07-14 18:23:40 +0700

    Delete DoAnWebsiteBanDoGiaDung directory

--- DoAnWebsiteBanDoGiaDung/DoAnWebsiteBanDoGiaDung/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DoAnWebsiteBanDoGiaDung.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
